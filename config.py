4ce7268f5eca5ab4f39aa16946fbae3f6f36a632
Alienware <fahimi72@gmail.com>
2018-03-23 11:31:29 -0500

    second config - train

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ CONFIG = {
 	'saliency_test' : range(600,700),
 	'blur_sigma' : 3,
 	'first_blur_sigma': 0,
-	'gaussian_sigma' : 40,
+	'gaussian_sigma' : 20,
 	'mask_th' : 0.01,
 	'test_mask_th' : 0.5,
-	'distance': 120,
+	'distance': 150,
 	'encoder' : 'dvgg16',
 	'dataset_dir': '/media/ramin/data/scanpath/dataset/OSIE/',
 }
